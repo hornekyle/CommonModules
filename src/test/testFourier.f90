1d75573026da29b221d7a33d0f0500f922f3f9a7
Kyle Horne <horne.kyle@gmail.com>
2016-05-01 07:23:11 -0500

    Added iFFT test
    * Still need to create 2D transform tests

--- src/test/testFourier.f90
+++ src/test/testFourier.f90
@@ -1,7 +1,6 @@
 program testFourier_prg
 	!! Test program for fourier_mod  
 	!! @todo
-	!! Add tests for inverse transforms  
 	!! Add tests for 2d transforms  
 	!! Add tests for 2d inverse transforms
 	use kinds_mod
@@ -9,6 +8,7 @@ program testFourier_prg
 	implicit none
 	
 	call testFFT
+	call testIFFT
 	
 contains
 
@@ -16,14 +16,14 @@ contains
 		!! Test FFT to verify operation
 		logical,dimension(1)::results
 		
-		integer,parameter::N = 100
+		integer,parameter::N = 10000
 		real(wp),parameter::D = 5.0_wp
 		real(wp),dimension(N)::t,s,f,A
 		integer::k,kP
 		
 		forall(k=1:N) t(k) = D*(real(k-1,wp)/real(N-1,wp))
 		s = cos( (2.0_wp*PI)*t )
-		f = tFFT(t)
+		f = FFT_freq(t)
 		A = FFT(s)
 		kP = maxloc(A(:N/2),1)
 		
@@ -31,5 +31,26 @@ contains
 		
 		if( .not.all(results) ) error stop "Failed FFT check"
 	end subroutine testFFT
+
+	subroutine testIFFT
+		!! Test FFT to verify operation
+		logical,dimension(1)::results
+		
+		integer,parameter::N = 10000
+		real(wp),parameter::D = 5.0_wp
+		real(wp),dimension(N)::t,f
+		complex(wp),dimension(N)::s,fs,ffs
+		integer::k
+		
+		forall(k=1:N) t(k) = D*(real(k-1,wp)/real(N-1,wp))
+		s   = cos( (2.0_wp*PI)*t )
+		f   = FFT_freq(t)
+		fs  = FFT(s)
+		ffs = iFFT(fs)
+		
+		results(1) = norm2(abs(s-ffs))<1.0E-10_wp
+		
+		if( .not.all(results) ) error stop "Failed iFFT check"
+	end subroutine testIFFT
 
 end program testFourier_prg
