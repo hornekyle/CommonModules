5799dedd8932b32e2a9dd5cada7a352632371761
Kyle Horne <horne.kyle@gmail.com>
2016-05-02 09:37:13 -0500

    Added MatIO and started a test

---
+++ src/test/testMatIO.f90
@@ -0,0 +1,22 @@
+program testMatIO_prg
+	!! Test program for matIO_mod
+	use kinds_mod
+	use matIO_mod
+	implicit none
+	
+contains
+
+	subroutine testWriteMat
+		!! Verify operation of writeMat
+		integer,parameter::N = 5
+		integer,parameter::M = 4
+		real(wp),dimension(N,M)::A
+		
+		integer::i,j
+		
+		forall(i=1:N,j=1:M) A(i,j) = real(i+j,wp)
+		
+		call writeMat('data.mat','A',A)
+	end subroutine testWriteMat
+
+end program testMatIO_prg
