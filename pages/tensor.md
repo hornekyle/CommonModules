a184f31efba36cedfda643b1b8a6f1615f1b6731
Kyle Horne <horne.kyle@gmail.com>
2016-05-04 07:17:32 -0500

    Added tensor_mod and a basic test

---
+++ pages/tensor.md
@@ -0,0 +1,1 @@
+title: Tensor Instructions 
---
+++ src/test/testTensor.f90
@@ -0,0 +1,24 @@
+program testTensor_prg
+	!! Test program for Tensor_mod
+	use kinds_mod
+	use tensor_mod
+	implicit none
+	
+	call testDot
+	
+contains
+
+	subroutine testDot
+		!! Verify operation of Dot and Dyadic
+		real(wp),dimension(3)::u,v
+		real(wp),dimension(3,3)::D
+		real(wp)::r
+		
+		u = [1.0_wp,2.0_wp,0.0_wp]
+		v = [0.0_wp,2.0_wp,3.0_wp]
+		
+		r = u.o.v
+		D = u.d.v
+	end subroutine testDot
+
+end program testTensor_prg
