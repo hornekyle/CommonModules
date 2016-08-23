4a59ee3869615edd52f3cc835ce73462768da0d6
Kyle Horne <horne.kyle@gmail.com>
2016-08-23 10:17:35 -0500

    Added BiCGSTAB

--- src/matrix/solvers.f90
+++ src/matrix/solvers.f90
@@ -102,7 +102,7 @@ module solvers_mod
 	end interface
 	
 	!==================================!
-	!= conjugateGradient Declarations =!
+	!= ConjugateGradient Declarations =!
 	!==================================!
 	
 	type,extends(solver_t)::conjugateGradient_t
@@ -116,6 +116,21 @@ module solvers_mod
 		module procedure newConjugateGradient
 	end interface
 	
+	!=========================!
+	!= BiCGSTAB Declarations =!
+	!=========================!
+	
+	type,extends(solver_t)::biCGSTAB_t
+	contains
+		procedure::setup => setup_bcg
+		procedure::step  =>  step_bcg
+		procedure::solve => solve_bcg
+	end type
+	
+	interface biCGSTAB_t
+		module procedure newBiCGSTAB
+	end interface
+	
 	!===========!
 	!= Exports =!
 	!===========!
@@ -125,6 +140,7 @@ module solvers_mod
 	public::gaussSeidel_t
 	public::SOR_t
 	public::conjugateGradient_t
+	public::biCGSTAB_t
 	
 contains
 
@@ -136,7 +152,6 @@ contains
 		class(solver_t),intent(inout)::self
 		
 		call showProgress(self%name_long,0.0_wp)
-! 		write(*,*) 'Start: ',self%name_long
 		self%solveTime = cpuTime()
 	end subroutine startReport
 
@@ -145,19 +160,25 @@ contains
 		integer,intent(in)::k
 		real(wp),intent(in)::r
 		
+		character(:),allocatable::its
 		real(wp)::progress
+		
+		its = ' '//colorize(intToChar(k),[5,0,0])
 		
 		progress = log( r )/log(self%tol)
 		if(progress>=1.0_wp) progress = 0.995_wp
-		call showProgress(self%name_long,progress)
-! 		write(*,*) k,r,progress
+		call showProgress(self%name_long//its,progress,60)
 	end subroutine stepReport
 
-	subroutine stopReport(self)
+	subroutine stopReport(self,k)
 		class(solver_t),intent(inout)::self
+		integer,intent(in)::k
 		real(wp)::t0,t1
 		
-		call showProgress(self%name_long,1.0_wp)
+		character(:),allocatable::its
+		its = ' '//colorize(intToChar(k),[5,0,0])
+		
+		call showProgress(self%name_long//its,1.0_wp,60)
 		t0 = self%solveTime
 		t1 = cpuTime()
 		self%solveTime = t1-t0
@@ -227,7 +248,7 @@ contains
 			call self%stepReport(k,rss/rss0)
 			if(rss/rss0<self%tol) exit
 		end do
-		call self%stopReport()
+		call self%stopReport(k)
 	end function solve_jkb
 
 	!========================!
@@ -299,7 +320,7 @@ contains
 			call self%stepReport(k,rss/rss0)
 			if(rss/rss0<self%tol) exit
 		end do
-		call self%stopReport()
+		call self%stopReport(k)
 	end function solve_gs
 
 	!================!
@@ -373,12 +394,12 @@ contains
 			call self%stepReport(k,rss/rss0)
 			if(rss/rss0<self%tol) exit
 		end do
-		call self%stopReport()
+		call self%stopReport(k)
 	end function solve_sor
 
-	!================!
-	!= SOR Routines =!
-	!================!
+	!==============================!
+	!= ConjugateGradient Routines =!
+	!==============================!
 
 	function newConjugateGradient() result(self)
 		type(conjugateGradient_t)::self
@@ -391,7 +412,7 @@ contains
 		class(conjugateGradient_t),intent(inout)::self
 		class(sparse_t),intent(in)::A
 		
-		self%maxIts = A%N
+		self%maxIts = 2*A%N
 	end subroutine setup_cg
 	
 	function step_cg(self,A,b,x0,l) result(x)
@@ -405,10 +426,25 @@ contains
 		real(wp),dimension(:),allocatable::r
 		integer::k
 		
+		real(wp),dimension(:),allocatable::Ap,p
+		real(wp)::alpha,beta,rTr,rTr_old
+		
 		x = x0
 		r = b-matmul(A,x)
 		
+		p = r
+		rTr = dot_product(r,r)
 		
+		do k=1,l
+			Ap = matmul(A,p)
+			alpha = rTr/dot_product(p,Ap)
+			x = x+alpha*p
+			r = r-alpha*Ap
+			rTr_old = rTr
+			rTr = dot_product(r,r)
+			beta = rTr/rTr_old
+			p = r+beta*p
+		end do
 	end function step_cg
 	
 	function solve_cg(self,A,b,x0) result(x)
@@ -427,7 +463,7 @@ contains
 		
 		x = b/A%getDiagonal()
 		if(present(x0)) x = x0
-		
+			
 		r = b-matmul(A,x)
 		rss0 = norm2(r)
 		
@@ -449,7 +485,114 @@ contains
 			call self%stepReport(k,rss/rss0)
 			if(rss/rss0<self%tol) exit
 		end do
-		call self%stopReport()
+		call self%stopReport(k)
 	end function solve_cg
+
+	!=====================!
+	!= BiCGSTAB Routines =!
+	!=====================!
+
+	function newBiCGSTAB() result(self)
+		type(biCGSTAB_t)::self
+		
+		self%name_long = 'BiCGSTAB'
+		self%name_short = 'BCGSTB'
+	end function newBiCGSTAB
+
+	subroutine setup_bcg(self,A)
+		class(biCGSTAB_t),intent(inout)::self
+		class(sparse_t),intent(in)::A
+		
+		self%maxIts = 2*A%N
+	end subroutine setup_bcg
+	
+	function step_bcg(self,A,b,x0,l) result(x)
+		class(biCGSTAB_t),intent(inout)::self
+		class(sparse_t),intent(in)::A
+		real(wp),dimension(A%N),intent(in)::b
+		real(wp),dimension(A%N),intent(in)::x0
+		integer,intent(in)::l
+		real(wp),dimension(:),allocatable::x
+		
+		real(wp),dimension(:),allocatable::r
+		integer::k
+		
+		real(wp),dimension(:),allocatable::r0,p,s,Ap,As
+		real(wp)::orho,rho,alpha,beta,omega
+		
+		x = x0
+		r = b-matmul(A,x)
+		
+		rho   = 1.0_wp
+		alpha = 1.0_wp
+		omega = 1.0_wp
+		
+		r0 = r
+		Ap = [(0.0_wp,k=1,size(b))]
+		p  = [(0.0_wp,k=1,size(b))]
+		
+		do k=1,l
+			orho = rho
+			rho = dot_product(r0,r)
+			beta = (rho/orho)*(alpha/omega)
+			p = r+beta*(p-omega*Ap)
+			Ap = matmul(A,p)
+			alpha = rho/dot_product(r0,Ap)
+			s = r-alpha*Ap
+			As = matmul(A,s)
+			omega = dot_product(As,s)/dot_product(As,As)
+			x = x+alpha*p+omega*s
+			r = s-omega*As
+		end do
+	end function step_bcg
+	
+	function solve_bcg(self,A,b,x0) result(x)
+		class(biCGSTAB_t),intent(inout)::self
+		class(sparse_t),intent(in)::A
+		real(wp),dimension(A%N),intent(in)::b
+		real(wp),dimension(A%N),intent(in),optional::x0
+		real(wp),dimension(:),allocatable::x
+		
+		real(wp),dimension(:),allocatable::r
+		real(wp)::rss0,rss
+		integer::k
+		
+		real(wp),dimension(:),allocatable::r0,p,s,Ap,As
+		real(wp)::orho,rho,alpha,beta,omega
+		
+		x = b/A%getDiagonal()
+		if(present(x0)) x = x0
+		
+		r = b-matmul(A,x)
+		rss0 = norm2(r)
+		
+		rho   = 1.0_wp
+		alpha = 1.0_wp
+		omega = 1.0_wp
+		
+		r0 = r
+		Ap = [(0.0_wp,k=1,size(b))]
+		p  = [(0.0_wp,k=1,size(b))]
+		
+		call self%startReport()
+		do k=1,self%maxIts
+			orho = rho
+			rho = dot_product(r0,r)
+			beta = (rho/orho)*(alpha/omega)
+			p = r+beta*(p-omega*Ap)
+			Ap = matmul(A,p)
+			alpha = rho/dot_product(r0,Ap)
+			s = r-alpha*Ap
+			As = matmul(A,s)
+			omega = dot_product(As,s)/dot_product(As,As)
+			x = x+alpha*p+omega*s
+			r = s-omega*As
+			
+			rss = norm2(r)
+			call self%stepReport(k,rss/rss0)
+			if(rss/rss0<self%tol) exit
+		end do
+		call self%stopReport(k)
+	end function solve_bcg
 
 end module solvers_mod
--- src/test/testSparse.f90
+++ src/test/testSparse.f90
@@ -134,7 +134,7 @@ contains
 		real(wp),dimension(:),allocatable::T1
 		integer::N,i,s
 		
-		N  = 200
+		N  = 100
 		s  = N/30
 		allocate(x(0:N+1))
 		x  = linspace(0.0_wp,1.0_wp,N+2)
@@ -191,6 +191,10 @@ contains
 		T1(1:N) = solver%solve(A,q)
 		deallocate(solver)
 		
+		allocate(solver,source=biCGSTAB_t())
+		call solver%setup(A)
+		T1(1:N) = solver%solve(A,q)
+		deallocate(solver)
 		
 		call figure()
 		call subplot(1,1,1)
